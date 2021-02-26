330e5dba33d3a6daaf6f228448da05413c64f144
anastasialeonti <anastasia.leonti@atos.net>
2021-02-26 22:29:14 +0200

    register success

--- GeolocApi/Api/views.py
+++ GeolocApi/Api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('psw')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('success')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def success(request):
+    return render(request, 'success.html')
